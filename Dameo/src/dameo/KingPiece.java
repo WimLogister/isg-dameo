0395ea5ead9b319476215b95055ce6cedc7a70c7
WimLogister <wimlogister@hotmail.com>
2015-12-02 12:37:22 +0100

    King movement is now working. Due to polymorphism, a King can capture the same way a regular piece does until I implement King capturing.

--- Dameo/src/dameo/KingPiece.java
+++ Dameo/src/dameo/KingPiece.java
@@ -3,6 +3,7 @@ package dameo;
 import dameo.gametree.State;
 import dameo.move.Move;
 import dameo.move.SingleCaptureMove;
+import dameo.move.SingleMove;
 import java.awt.Point;
 import java.util.HashSet;
 import java.util.List;
@@ -25,6 +26,9 @@ public class KingPiece extends Piece {
 
     @Override
     public Set<SingleCaptureMove> generateCapturingMoves(State s, List<Point> capturedList) {
+        Piece[][] board = s.getBoard();
+        
+        Set<SingleCaptureMove> moves = new HashSet<>();
         return super.generateCapturingMoves(s, capturedList); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -43,49 +47,136 @@ public class KingPiece extends Piece {
         int relativeX = dir*col;
         int relativeY = dir*row;
         
-        /*
-        Construct non-jumping moves in relative coordinates.
-        */
-        final int relativeForward = checkY + 1;
-        final int relativeBackward = checkY - 1;
-        final int relativeLeft = checkX - 1;
-        final int relativeRight = checkX + 1;
-        
         boolean pieceReached = false;
         
-        int x_i = 1;
-        int y_i = 1;
-        
-        /* Check up */
-        while(relativeY+x_i <= color.getBoardTopEdge() && !pieceReached) {
-            
+        /*
+        Check up.
+        Start by checking we don't move off the board
+        */
+        while(++relativeY <= color.getBoardTopEdge() && !pieceReached) {
+            /* Check that square is empty */
+            if (board[relativeY*dir][col] == null) {
+                moves.add(new SingleMove(col, relativeY*dir, col, row));
+            }
+            /* Non-empty square reached, stop iterating in this direction */
+            else {
+                pieceReached = true;
+            }
         }
+        pieceReached = false;
+        relativeY = dir*row;
         
         /* Check down */
-        while(relativeY-x_i >= color.getBoardTopEdge() && !pieceReached) {
-            
+        while(--relativeY >= color.getBoardBottomEdge() && !pieceReached) {
+            /* Check that square is empty */
+            if (board[relativeY*dir][col] == null) {
+                moves.add(new SingleMove(col, relativeY*dir, col, row));
+            }
+            /* Non-empty square reached, stop iterating in this direction */
+            else {
+                pieceReached = true;
+            }
         }
+        pieceReached = false;
         
         /* Check left */
-        while(relativeX-x_i >= color.getBoardTopEdge() && !pieceReached) {
-            
+        while(--relativeX >= color.getBoardLeftEdge() && !pieceReached) {
+            /* Check that square is empty */
+            if (board[row][relativeX] == null) {
+                moves.add(new SingleMove(relativeX*dir, row, col, row));
+            }
+            /* Non-empty square reached, stop iterating in this direction */
+            else {
+                pieceReached = true;
+            }
         }
+        relativeX = dir*col;
+        pieceReached = false;
         
         /* Check right */
-        while(relativeY+x_i <= color.getBoardTopEdge() && !pieceReached) {
-            
+        while(++relativeX <= color.getBoardRightEdge() && !pieceReached) {
+            /* Check that square is empty */
+            if (board[row][relativeX] == null) {
+                moves.add(new SingleMove(relativeX*dir, row, col, row));
+            }
+            /* Non-empty square reached, stop iterating in this direction */
+            else {
+                pieceReached = true;
+            }
         }
         
-        /* Now check diagonals */
+        /* Reset relative coordinates for checking diagonals */
+        relativeY = dir*row;
+        relativeX = dir*col;
+        pieceReached = false;
         
         /* Check up and right */
+        while(++relativeX <= color.getBoardRightEdge() &&
+                ++relativeY <= color.getBoardTopEdge() && !pieceReached) {
+            /* Check that square is empty */
+            if (board[relativeY][relativeX] == null) {
+                moves.add(new SingleMove(relativeX*dir, relativeY*dir, col, row));
+            }
+            /* Non-empty square reached, stop iterating in this direction */
+            else {
+                pieceReached = true;
+            }
+        }
+        
+        /* Reset relative coordinates for checking next diagonal */
+        relativeY = dir*row;
+        relativeX = dir*col;
+        pieceReached = false;
         
         /* Check down and right */
+        while(++relativeX <= color.getBoardRightEdge() &&
+                --relativeY >= color.getBoardBottomEdge()&& !pieceReached) {
+            /* Check that square is empty */
+            if (board[relativeY][relativeX] == null) {
+                moves.add(new SingleMove(relativeX*dir, relativeY*dir, col, row));
+            }
+            /* Non-empty square reached, stop iterating in this direction */
+            else {
+                pieceReached = true;
+            }
+        }
+        
+        /* Reset relative coordinates for checking next diagonal */
+        relativeY = dir*row;
+        relativeX = dir*col;
+        pieceReached = false;
         
         /* Check down and left */
+        while(--relativeX >= color.getBoardLeftEdge() &&
+                --relativeY >= color.getBoardBottomEdge() && !pieceReached) {
+            /* Check that square is empty */
+            if (board[relativeY][relativeX] == null) {
+                moves.add(new SingleMove(relativeX*dir, relativeY*dir, col, row));
+            }
+            /* Non-empty square reached, stop iterating in this direction */
+            else {
+                pieceReached = true;
+            }
+        }
+        
+        /* Reset relative coordinates for checking next diagonal */
+        relativeY = dir*row;
+        relativeX = dir*col;
+        pieceReached = false;
         
         /* Check up and left */
-        return super.generateSingleMoves(s); //To change body of generated methods, choose Tools | Templates.
+        while(--relativeX >= color.getBoardLeftEdge() &&
+                ++relativeY <= color.getBoardTopEdge() && !pieceReached) {
+            /* Check that square is empty */
+            if (board[relativeY][relativeX] == null) {
+                moves.add(new SingleMove(relativeX*dir, relativeY*dir, col, row));
+            }
+            /* Non-empty square reached, stop iterating in this direction */
+            else {
+                pieceReached = true;
+            }
+        }
+        return moves;
     }
     
     
